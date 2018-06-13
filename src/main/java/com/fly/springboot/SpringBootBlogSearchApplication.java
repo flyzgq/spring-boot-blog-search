365ec72135a816de044a543ddced253268b17113
fly <zgqtsyx@163.com>
2018-06-14 00:38:03 +0800

    Initial commit

---
+++ src/main/java/com/fly/springboot/SpringBootBlogSearchApplication.java
@@ -0,0 +1,12 @@
+package com.fly.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogSearchApplication.class, args);
+    }
+}
